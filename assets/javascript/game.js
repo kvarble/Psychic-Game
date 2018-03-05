b883bd5e87d188256a4aed57d7706cd60dc60ed7
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-03-05 15:30:03 -0600

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,77 @@
-function GetLetter()
-{
-    var letters = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"];
-    console.log(letters[4]);
-    var wins = 0;
-    var losses = 0;
-    var guesses = 0;
+var computerGuess;
+        var playerGuess = 0;
+        var Guesses = 9;
+        var wins = 0;
+        var losses = 0;
+        var incorrect = [];
 
-    document.onkeyup = function () {
-        var playerGuess = String.fromCharCode(event.keyCode)
-            toLowerCase();
-            console.log(userguess);
+    function gameStart() {
 
-    var computerGuess = options[Math.floor(Math.random()*letters.length)];
-    
-    document.getElementById("message").innerHTML=random;
-    console.log(computerGuess);
-}
+// computer starts game by picking letter from array
+        var letters = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ];
+        var computerGuess = letters[Math.floor(Math.random()*letters.length)];
+        var guesses = 9;
+        console.log(computerGuess);
+        // player guesses letter creates an event everytime key is pressed
 
-for (i=0; i<9; i++) {
-    guess = document.write("what's your guess?");
-    if (answer == guess) {
-        document.write("correct!");
-        break;
-    }else{
-        guess = document.write("nope!");
+        document.onkeyup = function(event) {
+            var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+            incorrect.push(event.key);
+        // create if/else statements for correct/incorrect letters guessed and restart game
+
+        if (playerGuess === computerGuess)
+        {wins++
+            alert("you read my mind!")
+            incorrect = [];
+            gameStart(); 
+        }
+        else {
+            guesses --;
+            if (guesses === 0) {losses ++ 
+            alert ("you cannot read minds!");
+            incorrect = [];
+            
+            gameStart()
+            }
+           
+        }
+// display wins, losses, remaining guesses, and letters chosen
+    var html = "<p> Can you read my mind? Guess a letter! </p>" +
+                "<p> wins: " + wins + "</p>" +
+                "<p> losses: " + losses + "</p>" +
+                "<p> guesses: " + incorrect + "</p>" +
+                "<p> remaining guesses: " + guesses + "</p>"
+
+    document.querySelector("#game").innerHTML = html;
+
+        };
+
     }
-}
-
-}
+gameStart();
